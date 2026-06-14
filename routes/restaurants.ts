951c5a6cc4b6df4feb2c302d441cb4fe8c526a83
Rohit Soni <dynamicsoni.rohit@gmail.com>
2026-06-14 11:33:12 +0530

    Step 3: Setup zod and middlewares intro

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,8 +1,26 @@
 import express from 'express';
+import { validate } from '../middlewares/validate.js';
+import { RestaurantSchema, type TRestaurant } from '../schemas/restaurants.js';
+import { nanoid } from 'zod';
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Restaurants');
+router.post('/', validate(RestaurantSchema), async (req, res) => {
+  try {
+    const restaurant = req.body as TRestaurant;
+    const newRestaurant = {
+      id: nanoid("10"),
+      ...restaurant,
+      reviews: [],
+      rating: 0,
+      totalReviews: 0,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
+
+  } catch (error) {
+
+  }
 });
 
 export default router;
